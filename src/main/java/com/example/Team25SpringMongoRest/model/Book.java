ef73505911b628cc56a963f06aa43bb0bbcfb3c4
russovicki01 <97059667+russovicki01@users.noreply.github.com>
2023-11-06 20:08:39 -0500

    Add files via upload

---
+++ src/main/java/com/example/Team25SpringMongoRest/model/Book.java
@@ -0,0 +1,34 @@
+package com.example.test.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "book")
+public class Book {
+    @Id
+    private String id;
+    private String title;
+    private String author;
+    private String genre;
+    private String publisher;
+    private double price;
+    private int copiesSold;
+    private double rating;
+    private double discountPercent;
+
+
+
+    // Constructors, getters, and setters
+}
