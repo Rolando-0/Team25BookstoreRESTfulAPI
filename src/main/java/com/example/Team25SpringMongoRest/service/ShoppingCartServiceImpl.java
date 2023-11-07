97b6d1dfdd9d15b3d452af13f12ddaea74f6fdb9
JenniferRuano <144237419+JenniferRuano@users.noreply.github.com>
2023-11-06 20:11:13 -0500

    Update ShoppingCartServiceImpl.java

--- src/main/java/com/example/Team25SpringMongoRest/service/ShoppingCartServiceImpl.java
+++ src/main/java/com/example/Team25SpringMongoRest/service/ShoppingCartServiceImpl.java
@@ -1,14 +1,16 @@
-package com.example.Team25SpringMongoRest.service;
+package com.example.shoppingcart.service;
 
-import com.example.Team25SpringMongoRest.model.BookDetails.Book;
-import com.example.Team25SpringMongoRest.model.ShoppingCart.ShoppingCart;
-import com.example.Team25SpringMongoRest.repository.ShoppingCartRepository;
-import com.example.Team25SpringMongoRest.repository.BookRepository;
+import com.example.shoppingcart.model.Book;
+import com.example.shoppingcart.model.ShoppingCart;
+import com.example.shoppingcart.repository.ShoppingCartRepository;
+import com.example.shoppingcart.repository.BookRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class ShoppingCartServiceImpl implements ShoppingCartService {
@@ -20,31 +22,48 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
     private BookRepository bookRepository;
 
     @Override
-    public double getSubtotal(String userId) {
+    public Double getSubtotal(String userId) {
         ShoppingCart cart = shoppingCartRepository.findByUserId(userId);
         if (cart == null) {
             throw new RuntimeException("Shopping cart not found for user: " + userId);
         }
-        return cart.getBooks().stream().mapToDouble(Book::getPrice).sum();
+        double subtotal = cart.getBooks().stream()
+                .mapToDouble(book -> book.getQuantity() * book.getPrice())
+                .sum();
+
+        return subtotal;
     }
 
     @Override
     public String addToCart(String userId, String bookId) {
         ShoppingCart cart = shoppingCartRepository.findByUserId(userId);
         if (cart == null) {
-            throw new RuntimeException("Shopping cart not found for user: " + userId);
+            cart = new ShoppingCart();
+            cart.setUserId(userId);
+            //throw new RuntimeException("Shopping cart not found for user: " + userId);
         }
 
         try {
-            Book bookToAdd = bookRepository.findById(bookId)
-                    .orElseThrow(() -> new NoSuchElementException("Book not found with ID: " + bookId));
-
+            //Uncomment when putting code together
+           // Optional<Book> bookToAdd = bookRepository.findById(bookId)
+                    //.orElseThrow(() -> new NoSuchElementException("Book not found with ID: " + bookId));
+            //Remove bottom four lines when putting code together
+            Book book1 = new Book();
+            Book book2 = new Book();
+            book1.setId(bookId);
+            book1.setName("Test");
+            book1.setPrice(20.00);
+            if(cart.getBooks() == null){cart.setBooks(new ArrayList<Book>());}
             // Check if the book is already in the cart
-            if (!cart.getBooks().contains(bookToAdd)) {
-                cart.getBooks().add(bookToAdd);
+            book2 = cart.getBooks().stream().filter(book -> book.getId().equals(bookId)).findAny().orElse(null);
+            if (book2==null) {
+                book1.setQuantity(1);
+                cart.getBooks().add(book1);
                 shoppingCartRepository.save(cart);
                 return "Book added to the shopping cart successfully";
             } else {
+                book2.setQuantity(book2.getQuantity() +1);
+                shoppingCartRepository.save(cart);
                 return "Book is already in the shopping cart. Quantity increased.";
             }
         } catch (NoSuchElementException e) {
@@ -69,12 +88,20 @@ public class ShoppingCartServiceImpl implements ShoppingCartService {
         }
 
         try {
-            Book bookToRemove = bookRepository.findById(bookId)
-                    .orElseThrow(() -> new NoSuchElementException("Book not found with ID: " + bookId));
+            Book bookToRemove = cart.getBooks().stream()
+                    .filter(book -> book.getId().equals(bookId))
+                    .findFirst()
+                    .orElse(null);
 
-            // Remove the book from the cart
-            boolean removed = cart.getBooks().remove(bookToRemove);
-            if (removed) {
+            if (bookToRemove != null) {
+                if (bookToRemove.getQuantity() > 1) {
+                    // Decrease the quantity by one
+                    bookToRemove.setQuantity(bookToRemove.getQuantity() - 1);
+                } else {
+                    // Remove the book from the cart when the quantity is zero
+                    cart.getBooks().remove(bookToRemove);
+                }
+
                 shoppingCartRepository.save(cart);
                 return "Book removed from the shopping cart successfully";
             } else {
