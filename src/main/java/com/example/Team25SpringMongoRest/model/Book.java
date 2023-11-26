67bf671bb33ad5d52142ec2352ae7162021a7527
russovicki01 <97059667+russovicki01@users.noreply.github.com>
2023-11-26 11:48:51 -0500

    Update Book.java

--- src/main/java/com/example/Team25SpringMongoRest/model/Book.java
+++ src/main/java/com/example/Team25SpringMongoRest/model/Book.java
@@ -5,18 +5,14 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.core.mapping.Field;
-
-import java.math.BigDecimal;
-
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Document(collection = "book")
 public class Book {
+
     @Id
     private String id;
     private String title;
@@ -26,7 +22,5 @@ public class Book {
     private double price;
     private int copiesSold;
     private double rating;
-    private double discountPercent;
-    private Integer quantity;
 
 }
