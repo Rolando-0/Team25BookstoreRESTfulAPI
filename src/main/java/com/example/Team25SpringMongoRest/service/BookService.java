10704dafef846cd4ce44734532a4573bfcc2c7e6
russovicki01 <97059667+russovicki01@users.noreply.github.com>
2023-11-26 11:50:13 -0500

    Update BookService.java

--- src/main/java/com/example/Team25SpringMongoRest/service/BookService.java
+++ src/main/java/com/example/Team25SpringMongoRest/service/BookService.java
@@ -23,3 +23,48 @@ public class BookService {
 
  }
 }
+
+package com.example.test.service;
+
+import com.example.test.model.Book;
+import com.example.test.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class BookService {
+
+    @Autowired
+    private BookRepository bookRepository;
+    public Book addBook(Book book) {
+        return bookRepository.save(book);
+    }
+    public List<Book> getBooksByGenre(String genre) {
+        return bookRepository.findByGenre(genre);
+    }
+    public List<Book> findBooksByRating(double rating) {
+        return bookRepository.findByRating(rating);
+    }
+    public List<Book> getTopSellers() {
+        return bookRepository.findTop10ByOrderByCopiesSoldDesc();
+    }
+    public List<Book> getBooksByPublisher(String publisher) {
+        return bookRepository.findByPublisher(publisher);
+    }
+    public void updatePricesByPublisher(String publisher, double discountPercent) {
+        List<Book> books = bookRepository.findByPublisher(publisher);
+        for (Book book : books) {
+            double discountedPrice = book.getPrice() * (1 - discountPercent / 100);
+            book.setPrice(discountedPrice);
+            bookRepository.save(book);
+        }
+    }
+}
+
+
+
+
+
+
+
