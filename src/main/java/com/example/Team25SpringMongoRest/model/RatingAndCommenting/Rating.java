40575b42b9053c3312ba81201b651224c49f7e05
aminsalem02 <144493760+aminsalem02@users.noreply.github.com>
2023-11-27 21:31:12 -0500

    Add files via upload

---
+++ src/main/java/com/example/Team25SpringMongoRest/model/RatingAndCommenting/Rating.java
@@ -0,0 +1,38 @@
+package com.example.ratingsandcomments.model.RatingAndCommenting;
+
+import com.example.ratingsandcomments.model.Book;
+import com.example.ratingsandcomments.model.User;
+
+import javax.persistence.*;
+
+import lombok.*;
+
+import java.text.SimpleDateFormat;
+
+@Entity
+@Table(name = "Rating")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Rating {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "ratingID")
+    private Long ratingID;
+
+    @ManyToOne
+    @JoinColumn(name = "user")
+    private String user;
+
+    @ManyToOne
+    @JoinColumn(name= "bookID")
+    private String book;
+
+    @Column(name = "Rating")
+    private Integer rating;
+
+
+}
