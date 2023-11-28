e4f802037082db0aa8844ba4718bb5a4120d4c96
aminsalem02 <144493760+aminsalem02@users.noreply.github.com>
2023-11-27 21:26:47 -0500

    Add files via upload

--- src/main/java/com/example/Team25SpringMongoRest/repository/RatingRepository.java
+++ src/main/java/com/example/Team25SpringMongoRest/repository/RatingRepository.java
@@ -1,10 +1,15 @@
 package com.example.ratingsandcomments.repository;
 
+import com.example.ratingsandcomments.model.Book;
 import com.example.ratingsandcomments.model.Rating;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface RatingRepository extends MongoRepository<Rating, Long> {
+    List<Rating> findByBook (Book book);
 
 }
